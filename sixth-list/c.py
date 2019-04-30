ed9f2912d7022444149141cd823ef625888f9699
Antunes Dantas <antunesdantass@gmail.com>
2019-04-30 13:43:03 -0300

    finalizando questao 6 c

--- sixth-list/c.py
+++ sixth-list/c.py
@@ -1,28 +1,28 @@
 # Dominos 2
+vis = set()
+adj = {}
 
-def verifyFalls(adj, node):
-    count = 1
-    while node in adj:
-        count += 1
-        node = adj[node][0]
-    
-    return count
+def dfs(node):
+    vis.add(node)
+    for vizinho in adj[node]:
+        if vizinho not in vis:
+            dfs(vizinho)
 
 
 nOfTests = int(input())
 answers = []
 for i in range(nOfTests):
     n, m, l = list(map(int, input().split()))
-    adj = {}
+    for domino in range(1, n + 1):
+        adj[domino] = []
     for vertex in range(m):
         head, tail = list(map(int, input().split()))
-        if head not in adj:
-            adj[head] = []
         adj[head].append(tail)
-    total = 0
     for fall in range(l):
-        total += verifyFalls(adj, int(input()))
-    answers.append(total)
+        dfs(int(input()))
+    answers.append(len(vis))
+    vis = set()
+    adj = {}
 
 for answer in answers:
     print(answer)
