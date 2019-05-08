3533bba2945c8889d4ddcf43eea19d46730e121c
Antunes Dantas <antunesdantas@MacBook-Air-de-Antunes.local>
2019-05-07 22:48:41 -0300

    adicionando union find na questao a

--- seventh-list/a.py
+++ seventh-list/a.py
@@ -1,4 +1,20 @@
 # Itinerary of Santa Claus
+import random
+
+def find(parent, i):
+    if parent[i] == i:
+        return i
+    else:
+        return find(parent, parent[i])
+
+def union(parent, rank, x, y):
+    xroot = find(parent, x)
+    yroot = find(parent, y)
+    side = random.random() > 0.5
+    if side:
+        parent[xroot] = yroot
+    else:
+        parent[yroot] = xroot
 
 n, m = raw_input().split()
 while n != 0 and m != 0:
